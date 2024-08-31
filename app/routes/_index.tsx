86dc8df475f0231b7f68abd51d67fe9c26e91606
josemlrod <mrjosemlrod@gmail.com>
2024-08-31 19:43:10 -0400

    add tailwindcss + shadcnui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,8 +11,11 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
+      <h1 className="text-3xl font-thin">Welcome to Remix</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
+        <Button variant="default" onClick={() => console.log("roosevelt")}>
+          Feels right
+        </Button>
         <li>
           <a
             className="text-blue-700 underline visited:text-purple-900"
